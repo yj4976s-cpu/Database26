d5a1e0a2bb08d4446fc8a10765252e9d83c04910
320-21\yjs4976 <yj4976s@gmail.com>
2026-01-29 17:52:36 +0900

    LMS Member 실습

--- LMS_MBC.sql
+++ LMS_MBC.sql
@@ -48,3 +48,135 @@ UPDATE members set password = '1111' WHERE uid = 'kkw';
 # 회원삭제
 DELETE FROM members WHERE uid = 'kkw';
 UPDATE members set active = false WHERE uid = 'kkw'; # 회원비활성화
+
+drop table scores;
+create table scores (
+    id 		   INT AUTO_INCREMENT PRIMARY KEY,
+	member_id  INT NOT NULL,
+	korean 	   INT NOT NULL,
+    english	   INT NOT NULL,
+	math	   INT NOT NULL,
+    total	   INT NOT NULL,
+    average	   FLOAT NOT NULL,
+    grade	   CHAR(1) NOT NULL,
+    create_at  	DATETIME DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (member_id) REFERENCES members(id)
+    # 외래키생성	 내가갖은 필드와 연결		테이블 필드
+    );
+
+# 후보키: 공백이 없고, 유일해야 되는 필드들(학번, 주민번호, id, email.....)
+# PRIMARY KEY는 기본키로 공백이 없고, 유일해야 되고, 인덱싱이 되어 있는 옵션
+# 인덱싱 : db에서 빠른 찾기를 위한 옵션
+# 외래키(FOREIGN KEY) : 다른 테이블과 연결이 되는 키
+# 외래키는 자식이고 기본키는 부모
+# members가 부모임으로 kkw 계정이 있어야 scores 테이블에 점수를 넣을 수 있다.
+# members 테이블의 id와 scores 테이블에 member_id는 타입 일치 필수
+
+INSERT INTO scores (member_id, korean, english, math, total, average, grade)
+values(2,99,99,99,297,99,'A');
+INSERT INTO scores (member_id, korean, english, math, total, average, grade)
+values(3,88,88,88,264,88,'B');
+INSERT INTO scores (member_id, korean, english, math, total, average, grade)
+values(4,77,77,77,231,77,'C');
+INSERT INTO scores (member_id, korean, english, math, total, average, grade)
+values(5,66,66,66,198,66,'F');
+INSERT INTO scores (member_id, korean, english, math, total, average, grade)
+values(6,80,80,80,240,80,'B');
+INSERT INTO scores (member_id, korean, english, math, total, average, grade)
+values(8,90,90,90,270,90,'A');
+
+SELECT * FROM scores; # 전체 출력
+
+# 기본 정보 조회(INNER JOIN)
+# 성적 데이터가 존재하는 회원만 조회합니다. 이름, 과목 점수, 평균, 등급을 가져오는 쿼리.
+SELECT
+	m.name AS 이름,
+    m.uid  AS 아이디,
+    s.korean AS 국어,
+    s.english AS 영어,
+    s.math AS 수학,
+    s.total AS 총점,
+    s.average AS 평균,
+    s.grade AS 등급
+from members m
+# Aliasing(별칭): members m 처럼 테이블 이름 뒤에 한 글자 별칭을 주면 쿼리가 훨씬 간결해짐.
+JOIN scores s ON m.id = s.member_id;
+# ON 조건: m.id = s.member_id와
+# 같이 두 테이블을 연결하는 핵심 키(PK-FK)를 정확히 지정.
+
+DELETE FROM scores WHERE member_id = 2;
+
+# 성적이 없는 회원도 포함 조회 (LEFT JOIN)
+# 성적표가 아직 작성되지 않은 회원까지 모두 포함하여 명단을 만들 때 사용합니다. 성적이 없으면 NULL로 표시됩니다.
+SELECT
+ m.name AS 이름,
+m.role AS 역할,
+s.average AS 평균,
+s.grade AS 등급,
+IFNULL(s.grade, '미산출') AS 상태 # 성적이 없으면 '미산출'표시
+
+FROM members m
+LEFT JOIN scores s ON m.id = s.member_id;
+
+drop table boards;
+CREATE TABLE boards (
+	id		  INT AUTO_INCREMENT PRIMARY KEY,
+	member_id INT NOT NULL,
+    title 	  VARCHAR(200) NOT NULL,
+    content	  TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (member_id) REFERENCES members(id)
+);
+
+INSERT INTO boards (member_id, title, content)
+VALUES (3, '제목1', '내용1');
+INSERT INTO boards (member_id, title, content)
+VALUES (4, '제목4', '내용4');
+INSERT INTO boards (member_id, title, content)
+VALUES (4, '제목4', '내용4');
+INSERT INTO boards (member_id, title, content)
+VALUES (5, '제목6', '내용6');
+
+SELECT* FROM boards; # 전체 출력
+
+# 게시글 목록 조회(INNER JOIN)
+SELECT
+	b.id AS 글번호,
+    b.title AS 제목,
+    m.name AS 작성자, -- members 테이블에서 가져옴
+    b.created_at AS 작성일
+FROM boards b
+INNER JOIN members m ON b.member_id = m.id
+ORDER BY b.created_at DESC; -- 최신글 순으로 정렬
+
+# 특정 사용자의 글만 조회 (WHERE 절 조합)
+SELECT
+	b.title,
+    b.content,
+    m.name AS 작성자, -- members 테이블에서 가져옴
+    b.created_at
+FROM boards b
+JOIN members m on b.member_id = m.id
+WHERE m.uid = 'lhj'; -- 특정 아이디를 가진 유저의 글만 필터링
+
+# 관리자용: 통계 조회 (GROUP BY조합)
+SELECT
+	m.name,
+    m.uid,
+    COUNT(b.id) AS 작성글수 # GROUP BY와 셋트
+FROM members m
+LEFT JOIN boards b ON m.id = b.member_id
+GROUP BY m.id;
+
+# 작성자 이름으로 검색하기 (LIKE 활용)
+SELECT
+	b.id AS 글번호,
+	b.title AS 제목,
+    m.name AS 작성자,
+    b.created_at AS 작성일
+FROM boards b
+INNER JOIN members m ON b.member_id = m.id
+WHERE m.name LIKE '%효정%'
+ORDER BY b.created_at DESC;
